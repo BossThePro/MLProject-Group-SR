07e4ecf320aa45e5a6fcc880d68373b6e2f1e9fd
takhDS <khattaktahakhan@gmail.com>
2025-11-25 19:23:53 +0100

    Optimized decisionTree, better run time

--- sourceCode/decisionTree/decisiontree.py
+++ sourceCode/decisionTree/decisiontree.py
@@ -15,14 +15,15 @@ class DecisionTreeRegressor():
         self.max_depth = max_depth
         self.max_leaf_samples = max_leaf_samples #not implemented yet
         self.min_sample_split = min_sample_split
-        self.max_leaf = max_leaf
+        self.max_leaf = max_leaf #maximum number of leafs in the tree
         self.n_leaf = 0
         self.root = None
 
     def rss(self,data_points):
         """ For a given list of data points in a region, 
             this function returns the residual sum of squares for that region"""
-        RSS = np.sum((data_points - np.mean(data_points))**2) #For each region our prediction is the mean of points in that R
+        mean = np.mean(data_points)
+        RSS = np.sum((data_points - mean)**2) #For each region our prediction is the mean of points in that R
         return RSS
 
 
@@ -30,31 +31,30 @@ class DecisionTreeRegressor():
         """For given training data, all in the same region, this function
             returns the best feature to split on as well as the best threshold, 
             i.e one that minimizes RSS at that point"""
-        
-        n_samples,n_features = x.shape
 
-        if n_samples <= self.min_sample_split: 
-            return [None,None,None]
+        n_samples,n_features = x.shape
+        
+        X = np.array(x)
+        Y = np.array(y)
         
         best_rss = float('inf')
         best = [None,None,best_rss]
         for i in range(n_features):  #loop over all features 
             feature = x.columns[i]
-            splits = np.unique(x[feature]) # for each feature consider all possible splits
+            splits = np.unique(X[:,i]) # for each feature consider all possible splits
             
-
             for j in range(len(splits)-1): #loops over all possible splits in that feature
                 split = (splits[j] + splits[j+1]) / 2
                 
                 #split the data on both X and Y 
-                left_region = x[feature] < split
-                right_region = x[feature] >= split
+                left_region = X[:,i] < split
+                right_region = X[:,i] >= split
                 
-                left_labels = y[left_region]
-                right_labels = y[right_region]
+                left_labels = Y[left_region]
+                right_labels = Y[right_region]
                 
                 #calculate the RSS and add it for both regions
-                current_rss = self.rss(list(left_labels)) + self.rss(list(right_labels))
+                current_rss = self.rss(left_labels) + self.rss(right_labels)
                 
                 #if we have found a RSS less than previous best then we have a new lowest RSS
                 if current_rss < best_rss:
@@ -103,12 +103,13 @@ class DecisionTreeRegressor():
             return self.traverse(x,node.left_child)
         else:
             return self.traverse(x,node.right_child)
-        
+    
     def predict(self, X):
         """Predict class labels for samples in an array X"""
-        predictions = np.array()
-        for x in X:
-            predictions.append(self.traverse(x,self.root))
+        predictions = []
+        for i in range(len(X)):
+            row = X.iloc[i] 
+            predictions.append(self.traverse(row, self.root))
         return predictions
         
 
